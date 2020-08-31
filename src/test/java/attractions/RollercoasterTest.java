fed878c365422c22107adcc7655e6af244dc2bdc
James Monks <J.monks23@gmail.com>
2020-08-31 20:13:49 +0100

    rollercoaster tests passing

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,5 +28,40 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void visitorOver13And146cmIsAllowed() {
+        Visitor visitor = new Visitor(14, 146, 10);
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorUnder146cmIsNotAllowed() {
+        Visitor visitor = new Visitor(13, 1.45, 10);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorUnder13IsNotAllowed() {
+        Visitor visitor = new Visitor(12, 146, 10);
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(8.40, rollerCoaster.defaultPrice(), 0.5);
+    }
+
+    @Test
+    public void chargesStandardPriceUpTo2m() {
+        Visitor visitor = new Visitor(26, 2.0, 10);
+        assertEquals(8.40, rollerCoaster.priceFor(visitor), 0.5);
+    }
+
+    @Test
+    public void chargesDoubleOver2m() {
+        Visitor visitor = new Visitor(26, 2.1, 10);
+        assertEquals(16.80, rollerCoaster.priceFor(visitor), 0.05);
     }
 }
